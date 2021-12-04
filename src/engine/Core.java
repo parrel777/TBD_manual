b68c842143cbf8016cedb22afe7882931cc36cf3
parrel777 <nesi09@naver.com>
2021-12-04 23:00:08 +0900

    width edit

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -23,7 +23,7 @@ import screen.TitleScreen;
 public final class Core {
 
 	/** Width of current screen. */
-	private static final int WIDTH = 448;
+	private static final int WIDTH = 450;
 	/** Height of current screen. */
 	private static final int HEIGHT = 520;
 	/** Max fps of current screen. */
